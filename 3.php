a45e2c09954f94af7148b79a7df2d6bbe0b9b828
CodeExplorerAL <AllisonDevLee@gmail.com>
2024-08-28 01:31:34 +0800

    String and Variable

---
+++ 3.php
@@ -0,0 +1,7 @@
+<?php
+$weight = 76;
+echo 'CodeExploreAL\'s weight = $weight kg<br/>';
+echo "CodeExploreAL's weight = " . $weight . ' kg<br/>';  // 字串相接
+echo "weight = $weight kg<br/>";        //? 會有空格
+echo "weight = {$weight}kg<br/>";       //-> 使用大括號包住變數
+echo "CodeExploreAL's weight = $weight kg";
