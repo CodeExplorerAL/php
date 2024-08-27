c75295156f8f717a76bda5eb34eb4c7615448b83
CodeExplorerAL <AllisonDevLee@gmail.com>
2024-08-28 01:59:00 +0800

    Variable Types

---
+++ variable_types.php
@@ -0,0 +1,18 @@
+<script>
+  a = 10;
+  b = 3;
+  c = a + b;
+  document.write(c);
+</script>
+
+<hr />
+
+<?php
+
+$var = 'An';
+// $var = 12;
+// $var = 12.3;
+$var = true;
+
+echo gettype($var) . '<br/>';
+var_dump($var);
